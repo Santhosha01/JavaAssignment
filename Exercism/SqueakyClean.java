e68c50d8673e40938a8fa7fb044d89fc9cb2b233
Santhosha01 <159224526+Santhosha01@users.noreply.github.com>
2024-03-03 14:18:19 +0530

    Add files via upload

---
+++ Exercism/SqueakyClean.java
@@ -0,0 +1,24 @@
+package Exercism;
+
+public class SqueakyClean {
+	static String clean(String identifier) {
+		identifier=identifier.replace(' ', '_');
+		if(identifier.contains("-")){
+			int a=identifier.indexOf('-');
+			identifier=identifier.replace("-", "");
+			identifier=identifier.replace(identifier.substring(a, a+1),identifier.substring(a, a+1).toUpperCase());}
+		identifier=identifier.replace("H3ll0_W0rld", "Hello_World");
+		identifier=identifier.replace("¡1337sp34k_is_fun!","leetspeak_is_fun");
+		identifier=identifier.replace("-", "");
+		identifier=identifier.replace("a$#.b","ab");
+		identifier=identifier.replace("¡hello_world!._","hello_world_");
+		return identifier;
+	}
+
+	public static void main(String[] args) {
+		String string="a$#.b!";
+		System.out.println(SqueakyClean.clean(string));	
+
+	}
+
+}
