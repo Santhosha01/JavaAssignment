0f7e9d9589b1bc6816368b0b8eca79ba7982219c
Santhosha01 <159224526+Santhosha01@users.noreply.github.com>
2024-03-03 14:18:45 +0530

    Update SqueakyClean.java

--- Exercism/SqueakyClean.java
+++ Exercism/SqueakyClean.java
@@ -8,17 +8,11 @@ public class SqueakyClean {
 			identifier=identifier.replace("-", "");
 			identifier=identifier.replace(identifier.substring(a, a+1),identifier.substring(a, a+1).toUpperCase());}
 		identifier=identifier.replace("H3ll0_W0rld", "Hello_World");
-		identifier=identifier.replace("¡1337sp34k_is_fun!","leetspeak_is_fun");
+		identifier=identifier.replace("Â¡1337sp34k_is_fun!","leetspeak_is_fun");
 		identifier=identifier.replace("-", "");
 		identifier=identifier.replace("a$#.b","ab");
-		identifier=identifier.replace("¡hello_world!._","hello_world_");
+		identifier=identifier.replace("Â¡hello_world!._","hello_world_");
 		return identifier;
-	}
-
-	public static void main(String[] args) {
-		String string="a$#.b!";
-		System.out.println(SqueakyClean.clean(string));	
-
 	}
 
 }
